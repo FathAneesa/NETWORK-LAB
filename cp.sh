48e2070f1b2f11f766a11a65d31eb6d03d73d64f
FathAneesa <faathaneesa@gmail.com>
2025-05-05 15:23:01 +0530

    Add files via upload

---
+++ cp.sh
@@ -0,0 +1,34 @@
+#script to modify cp command
+
+if [ $# -ne 2 ]
+then
+ echo "\n Syntax is <$0><src file name><tgt filename>"
+exit 1
+fi
+
+if [ ! -f $1 ]
+then
+ echo "$1 is not existing or not an ordinary file"
+exit 2
+fi
+
+if [ -f $2 ]
+then
+ echo "target file exists,over write it (y/n)"
+read ans
+if [ $ans = "n" ]
+then
+exit 3
+fi
+
+sh cp.sh  tri.sh year.sh
+target file exists,over write it (y/n)
+y
+file copied
+
+
+sh cp.sh  tri.sh year.sh
+target file exists,over write it (y/n)
+n
+
+
