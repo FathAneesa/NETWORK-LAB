ee0c4e581727fc5226adee3784021fe3f1a173f4
FathAneesa <faathaneesa@gmail.com>
2025-05-15 12:51:15 +0530

    Add files via upload

---
+++ rename.sh
@@ -0,0 +1,15 @@
+#script to rename all ordinary files  with filename .pid
+
+if [ $# -ne 1 ]; then
+echo "syntax is $0 <directory location>"
+exit 1
+fi
+cd $1
+for file in *
+do
+if [ -f $file  ]; then
+mv $file $$$file
+fi
+done
+cd ..
+
