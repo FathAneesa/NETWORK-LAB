b443ab902c1bbd3be4236dd94e8252ccd50798b6
FathAneesa <faathaneesa@gmail.com>
2025-05-15 12:55:59 +0530

    Add files via upload

---
+++ reverse.sh
@@ -0,0 +1,19 @@
+#reverse the file without tac command
+
+if [ $# -ne 1 ]
+then
+echo "syntax is <$0><filename>"
+exit 1
+fi
+
+cp $1 file
+l=`cat file | wc -l`
+rm -f rfile
+while [ $l -ge 1 ]
+do
+tail -n 1 file >> rfile
+l=`expr $l - 1`
+head -n $l file > temp
+mv temp file
+done
+
